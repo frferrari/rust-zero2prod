8063952bee7bc010af97d26a9c57662c19dbcc7e
frferrari <f.ferrari@wanadoo.fr>
2022-11-03 18:17:52 +0100

    feat(docker): Optimize docker image size

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ cargo watch -x check -x test -x run
 docker build --tag zero2prod --file Dockerfile .
 
 cargo sqlx prepare -- --lib
+
+docker run -p 8000:8000 zero2prod
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
 use zero2prod::configuration::{get_configuration, Settings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::{PgPool, Pool, Postgres};
-use std::net::TcpListener;
 
 // cargo watch -x check -x run -x test
 // export ts=$(date +%s); curl -i v -X POST -d "email=thomas_mann_$ts@hostmail.com&name=Tom$ts" http://127.0.0.1:8000/subscriptions
@@ -15,11 +15,15 @@ async fn main() -> std::io::Result<()> {
 
     let configuration: Settings = get_configuration().expect("Failed to read configuration");
 
-    let db_pool: Pool<Postgres> = PgPool::connect_lazy(
-        &configuration.database.connection_string().expose_secret()
-    ).expect("Failed to create Postgres connection pool.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
 
-    let addr = format!("{}:{}",configuration.application.host, configuration.application.port);
+    let addr = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(addr)?;
-    run(listener, db_pool)?.await
+    run(listener, connection_pool)?.await
 }
